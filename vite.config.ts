ba8aa87b5dc108546e3dbfc493e0d0de2611b47d
williamuteich14 <williamuteich@gmail.com>
2025-07-05 19:53:45 -0300

    ajustandooooo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/sorriso-cachoeirinha-digital/' : '/',
+  base: '/ladingpage/',
   server: {
     host: "::",
     port: 8080,
