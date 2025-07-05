c7ea52d84312900599b5d638aff0bdcf73b0da5e
williamuteich14 <williamuteich@gmail.com>
2025-07-05 19:11:05 -0300

    cnfig de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  base: "/sorriso-cachoeirinha-digital/", 
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    componentTagger(),
-  ],
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  base: "/sorriso-cachoeirinha-digital"
+}));
